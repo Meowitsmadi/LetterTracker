ad348edd95293abbe641d94903f66d6d928111eb
Madison Kolley <53013984+Meowitsmadi@users.noreply.github.com>
2023-04-06 21:48:48 -0700

    Update SceneController.java

--- SceneController.java
+++ SceneController.java
@@ -1,11 +1,14 @@
 package application;
 
 import java.io.IOException;
+import java.sql.SQLException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.*;
 
 public class SceneController {
@@ -13,17 +16,56 @@ public class SceneController {
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
+	public LoginModel loginModel = new LoginModel();
+	
+	
+	@FXML 
+	private TextField txtPassword; 
 	
 	@FXML
-	public void switchToHomeScene(ActionEvent event) throws IOException{
-		
-		// check if PW is correct
-		
-		root = FXMLLoader.load(getClass().getResource("/view/LThomepage.fxml"));
+	private Label isConnected;
+	
+	public void Login(ActionEvent event) {
+		try {
+			// check if inputed password = DB password	
+			if (loginModel.checkLogin(1, txtPassword.getText())) 
+			{
+				isConnected.setText("Login success");
+			}
+			else if (txtPassword.getText().isEmpty()) 
+			{
+				isConnected.setText("Please enter a password");
+			}
+			else
+			{
+				isConnected.setText("Login fail");
+			}	
+		}
+		catch (SQLException e) {
+			isConnected.setText("Login fail");
+			e.printStackTrace();
+		}
+	}
+	
+	@FXML
+	public void switchToLoginScene(ActionEvent event) throws IOException, SQLException{
+		root = FXMLLoader.load(getClass().getResource("/view/Login.fxml"));
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
 		stage.setScene(scene);
 		stage.show();
+	}
+	
+	@FXML
+	public void switchToHomeScene(ActionEvent event) throws IOException, SQLException{
+		Login(event); // check if PW is correct
+		if (isConnected.getText().equals("Login success")) {
+			root = FXMLLoader.load(getClass().getResource("/view/LThomepage.fxml"));
+			stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+			scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+		}
 	}
 	
 	public void GoResetPassword(ActionEvent event) throws IOException {
