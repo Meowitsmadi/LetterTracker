bb0ecd77534c7bab39a0afbca474dc41ef9eba7b
William Ngo <willis12367129@gmail.com>
2023-04-24 19:04:26 -0700

    Update SceneController.java

--- SceneController.java
+++ SceneController.java
@@ -14,13 +14,18 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.*;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckMenuItem;
 import javafx.scene.control.ComboBox;
+import org.controlsfx.control.CheckListView;
 import javafx.scene.control.Label;
-import javafx.scene.control.SplitMenuButton;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.stage.*;
 import sql.sqliteDemo;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 
 // Defining the SceneController class which deals with switching to various scenes and implements the Initializable interface
 // for creating and populating Combo Boxes
@@ -45,23 +50,22 @@ public class SceneController implements Initializable {
 	@FXML
 	private ComboBox<String> programs = new ComboBox<String>();
 	
-	//@FXML
-	//private ComboBox<String> personalChar = new ComboBox<String>();
-	
-	//@FXML
-	//private ComboBox<String> academicChar = new ComboBox<String>();
-	
-	//@FXML
-	//private ComboBox<String> courses = new ComboBox<String>();
+	@FXML
+	private CheckListView<String> personalChar = new CheckListView<String>();
 	
 	@FXML
-	private SplitMenuButton personalChar = new SplitMenuButton();
+	private CheckListView<String> academicChar = new CheckListView<String>();
 	
 	@FXML
-	private SplitMenuButton academicChar = new SplitMenuButton();
+	private CheckListView<String> courses = new CheckListView<String>();
 	
+	public static TableView<StudentGrade> table_info_app;
+	public static ObservableList<StudentGrade> data_table;
 	@FXML
-	private SplitMenuButton courses = new SplitMenuButton();
+	private TableView<StudentGrade> table_info;
+	@FXML
+	private TableColumn<StudentGrade, String> column_course,column_grade;
+	
 	
 	// Text field containing password inputed by user
 	@FXML 
@@ -128,7 +132,7 @@ public class SceneController implements Initializable {
 	
 	// Method switches the scene to the Create New LOR page when the create new recommendation button is clicked
 	public void switchToHomeScene1(ActionEvent event) throws IOException{
-		
+			
 		root = FXMLLoader.load(getClass().getResource("/view/LThomepage.fxml"));
 		stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
@@ -170,20 +174,39 @@ public class SceneController implements Initializable {
 	public void initialize(URL location, ResourceBundle resources) {
 		genders.setItems(FXCollections.observableArrayList(sqliteDemo.getAllData("gender")));
 		semesters.setItems(FXCollections.observableArrayList(sqliteDemo.getAllData("semester")));
-		programs.setItems(FXCollections.observableArrayList(sqliteDemo.getAllData("programs")));
-		for (String s : sqliteDemo.getAllData("personalChara")) {
-			personalChar.getItems().add(new CheckMenuItem(s));
-		}
-				
-		for (String s : sqliteDemo.getAllData("academicChara")) {
-			academicChar.getItems().add(new CheckMenuItem(s));
-		}
-				
-		for (String s : sqliteDemo.getAllData("courses")) {
-			courses.getItems().add(new CheckMenuItem(s));
-		}
+		programs = ManageDataLORController.setAddData(programs, "programs");
+		courses = ManageDataLORController.setAddData(courses, "courses");
+		personalChar = ManageDataLORController.setAddData(personalChar, "personalChara");
+		academicChar = ManageDataLORController.setAddData(academicChar, "academicChara");
+		
+		table_info_app = table_info;
+		
+		//column_course.setCellValueFactory(new PropertyValueFactory<>("course"));
+		//column_grade.setCellValueFactory(new PropertyValueFactory<>("grade"));
+		
+		//loadData();
+		data_table=FXCollections.observableArrayList();
+		
+		//column_course.setCellFactory(TextFieldTableCell.forTableColumn());
+	    //column_course.setOnEditCommit(e->e.getTableView().getItems().get(e.getTablePosition().getRow()).setCourse(e.getNewValue()));
+
+	    //column_grade.setCellFactory(TextFieldTableCell.forTableColumn());
+	    //column_grade.setOnEditCommit(e->e.getTableView().getItems().get(e.getTablePosition().getRow()).setGrade(e.getNewValue()));
+	    
+	    //table_info.setEditable(true); 
 		
 	}
 	
+	private void loadData(){
+	    data_table=FXCollections.observableArrayList();
+
+	    for(int x=1;x< 12;x++){
+
+	    /* Generates the data items in the table */
+	    data_table.add(new StudentGrade("course "+x,"grade "+x));
+	    }
+
+	    table_info.setItems(data_table);
+	}
 
 }
