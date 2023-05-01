799cdd0d74df07013c940b6546965817ca568ab7
prathj <70483394+prathj@users.noreply.github.com>
2023-05-01 01:15:47 -0700

    Add files via upload

---
+++ application/ChangePassController.java
@@ -0,0 +1,64 @@
+package application;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.*;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.*;
+import sql.sqliteDemo;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ComboBox;
+
+public class ChangePassController {
+
+	private Stage stage;
+	private Scene scene;
+	private Parent root;
+	public PasswordModel passwordModel = new PasswordModel();
+	
+	@FXML
+	private TextField txtOldPassword; // text field where user enters new password
+	@FXML
+	private TextField txtNewPassword; // text field where user enters new password
+	@FXML
+	private TextField txtConfirmPassword; // text field where user confirms password
+	
+	@FXML private Label status;
+	
+	@FXML
+	public void SaveNewPassword(ActionEvent event) throws IOException, SQLException {
+		// checks if the new pasword is the default or not
+		if (txtNewPassword.getText().equals("p")) {
+			status.setText("You cannot use the default password again");
+		}
+		// checks if old password maatches teh one in the database AND new passwords amtch one another
+		else if (txtOldPassword.getText().equals(PasswordModel.GetOldPassword()) && txtNewPassword.getText().equals(txtConfirmPassword.getText()))
+		{
+			//change pass to inputed text
+			PasswordModel.ChangePassword(txtNewPassword.getText());
+			status.setText("Password successfully changed!");
+			
+			//go back to home screen after save
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/Login.fxml"));
+			root = loader.load();
+			SceneController sceneController = loader.getController();
+			sceneController.switchToLoginScene(event);
+		}
+		// what to do if old password does not match the one in the database
+		else if(!txtOldPassword.getText().equals(PasswordModel.GetOldPassword())) {
+			status.setText("Incorrect Old Password");
+		}
+		else
+		{
+			status.setText("Passwords do not match");
+		}	
+	}
+	
+	
+}
---
+++ application/LoginModel.java
@@ -0,0 +1,48 @@
+package application;
+import java.sql.*;
+
+import sql.sqliteDemo;
+
+public class LoginModel {
+	Connection conn;
+	
+	public LoginModel() {
+		conn = sqliteDemo.connect();
+		if (conn == null) 
+		{
+			System.out.println("DB did not connect.");
+			System.exit(1);
+		}
+	}
+	
+	// checks if the inputted password matches the one in the database
+	public boolean checkLogin(int userID, String password) throws SQLException {
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		
+		// selects from table where info is our inputed ID and pass
+		String query = "SELECT * FROM userInfo WHERE id = ? and info = ?"; 
+		try {
+			pst = conn.prepareStatement(query);
+			pst.setInt(1, userID);
+			pst.setString(2, password);
+			rs = pst.executeQuery(); //result of query saved in rs
+			
+			if (rs.next()) // user exists
+			{
+				return true;
+			}
+			else 
+			{
+				return false;
+			}	
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		} finally {
+			pst.close();
+			rs.close();
+		}
+	}
+}
