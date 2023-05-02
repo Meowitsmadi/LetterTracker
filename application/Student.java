00e98aa6cbed5c07748321fe3f252a0bc961f2ea
Madison Kolley <53013984+Meowitsmadi@users.noreply.github.com>
2023-05-01 21:57:48 -0700

    Update Student.java

--- application/Student.java
+++ application/Student.java
@@ -12,7 +12,16 @@ public class Student {
 	private StringProperty firstName;
 	private StringProperty lastName;
 	private String lor;
-	
+	private String gender;
+	private String firstSem;
+	private String program;
+	private String targetSchool;
+	private String firstYear;
+	private String currentDate;
+	private ArrayList<String> courses;
+	private ArrayList<String> courseGrades;
+	private ArrayList<String> personalChar;
+	private ArrayList<String> academicChar;
 	
 	public Student(int id, String firstName, String lastName) {
     	this.id = new SimpleIntegerProperty(id);
@@ -20,10 +29,23 @@ public class Student {
         this.lastName = new SimpleStringProperty(lastName);
     }
 	
-	public Student(String firstName, String lastName, String lor) {
+	public Student(String firstName, String lastName, String gender, String firstSem, String program, String targetSchool,
+			String firstYear, String currentDate, ArrayList<String> courses, ArrayList<String> courseGrades,
+			ArrayList<String> personalChar, ArrayList<String> academicChar, String lor) {
+		
         this.firstName = new SimpleStringProperty(firstName);
         this.lastName = new SimpleStringProperty(lastName);
 		this.lor = lor;
+		this.gender =  gender;
+		this.firstSem = firstSem;
+		this.program = program;
+		this.targetSchool = targetSchool;
+		this.firstYear = firstYear;
+		this.currentDate = currentDate;
+		this.courses = courses;
+		this.courseGrades = courseGrades;
+		this.personalChar = personalChar;
+		this.academicChar = academicChar;
     }
 	
     public Student() {}
@@ -50,4 +72,31 @@ public class Student {
 	public void setLastName(String value) { lastNameProperty().set(value); }
 	
 	public String getLOR() { return lor; }
+	
+	public String getGender() {return gender;}
+	public String getFirstSem() {return firstSem;}
+	public String getProgram() {return program;}
+	public String getTargetSchool() {return targetSchool;}
+	public String getFirstYear() {return firstYear;}
+	public String getCurrentDate() {return currentDate;}
+	public String getCourses() {
+		String out = "";
+		for (String course: courses)
+			{out += course + "/";}
+		return out;}
+	public String getCourseGrades() {
+		String out = "";
+		for (String grades: courseGrades)
+			{out += grades + "/";}
+		return out;}
+	public String getPersonalChar() {
+		String out = "";
+		for (String PChar: personalChar)
+			{out += PChar + "/";}
+		return out;}
+	public String getAcademicChar() {
+		String out = "";
+		for (String AChar: academicChar)
+			{out += AChar + "/";}
+		return out;}
 }
