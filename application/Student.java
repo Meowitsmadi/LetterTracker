e0e5d3494c727ebda61cb02f0d66da8e3b038cb2
Madison Kolley <53013984+Meowitsmadi@users.noreply.github.com>
2023-05-01 22:06:48 -0700

    Update Student.java

--- application/Student.java
+++ application/Student.java
@@ -18,8 +18,8 @@ public class Student {
 	private String targetSchool;
 	private String firstYear;
 	private String currentDate;
-	private ArrayList<String> courses;
-	private ArrayList<String> courseGrades;
+	private ArrayList<String> course;
+	private ArrayList<String> courseGrade;
 	private ArrayList<String> personalChar;
 	private ArrayList<String> academicChar;
 	
@@ -30,20 +30,26 @@ public class Student {
     }
 	
 	public Student(String firstName, String lastName, String gender, String firstSem, String program, String targetSchool,
-			String firstYear, String currentDate, ArrayList<String> courses, ArrayList<String> courseGrades,
-			ArrayList<String> personalChar, ArrayList<String> academicChar, String lor) {
+			String firstYear, String currentDate, ArrayList<String> course, ArrayList<String> courseGrade,
+			ArrayList<String> personalChar, ArrayList<String> academicChar) {
 		
         this.firstName = new SimpleStringProperty(firstName);
         this.lastName = new SimpleStringProperty(lastName);
-		this.lor = lor;
-		this.gender =  gender;
+		//this.lor = lor;
+		if(gender.equals("Male")) {
+            this.gender = "He";
+        }
+        else {
+            this.gender = "She";
+        }
+//		this.gender =  gender;
 		this.firstSem = firstSem;
 		this.program = program;
 		this.targetSchool = targetSchool;
 		this.firstYear = firstYear;
 		this.currentDate = currentDate;
-		this.courses = courses;
-		this.courseGrades = courseGrades;
+		this.course = course;
+		this.courseGrade = courseGrade;
 		this.personalChar = personalChar;
 		this.academicChar = academicChar;
     }
@@ -81,12 +87,12 @@ public class Student {
 	public String getCurrentDate() {return currentDate;}
 	public String getCourses() {
 		String out = "";
-		for (String course: courses)
-			{out += course + "/";}
+		for (String courses: course)
+			{out += courses + "/";}
 		return out;}
 	public String getCourseGrades() {
 		String out = "";
-		for (String grades: courseGrades)
+		for (String grades: courseGrade)
 			{out += grades + "/";}
 		return out;}
 	public String getPersonalChar() {
@@ -95,8 +101,75 @@ public class Student {
 			{out += PChar + "/";}
 		return out;}
 	public String getAcademicChar() {
+
+		
 		String out = "";
 		for (String AChar: academicChar)
 			{out += AChar + "/";}
 		return out;}
+
+	public String formatCourse() {
+	String out ="";
+	int i = 1;
+	out += String.format("%s also earned ", gender);
+	while(i<course.size()) {
+		if(i!=course.size()-1) { 
+			out+=String.format("%s from my %s, ", courseGrade.get(i), course.get(i));
+		}
+		else { out+=String.format("and %s from my %s", courseGrade.get(i), course.get(i)); }
+		i++;
+	}
+	return out+".\n";
+	}
+
+public String formatAcaChara() {
+	String out = String.format("%s %s",firstName.get(), academicChar.get(0));
+	for(int i=1; i<academicChar.size();i++) {
+		if(i!=course.size()-1) { 
+			out+= String.format("%s, ", academicChar.get(i));
+		}
+		else { out+= String.format("and %s", academicChar.get(i)); }	
+	}
+	return out+".\n\n";
+	
+}
+
+public String formatPerChara() {
+	String out = String.format("%s was always %s", gender, personalChar.get(0));
+	for(int i=1; i<personalChar.size();i++) {
+		if(i!=course.size()-1) { 
+			out+= String.format("%s, ", personalChar.get(i));
+		}
+		else { out+= String.format("and %s.\n", personalChar.get(i)); }	
+	}
+	return out+".\n\n";
+	
+}
+	
+	public String toString() {
+		String out = String.format(""
+			+ "Letter of Recommendation\r\n\n"
+			+ "For: %s %s\r\n"
+			+ "Date: %s\r\n"
+			+ "To: Graduate Admissions Committee\r\n\n"
+			+ "I am writing this letter to recommend my former student %s %s who is applying for the\r\n"
+			+ "%s program in your school. \r\n\n"
+			+ "I met %s in %s when he enrolled in my \"%s\" course.\r\n\n"
+			+ "%s earned \"%s\" from this tough course, and this shows how knowledgeable and hard worker %s is. \r\n", firstName.get(), lastName.get(), currentDate, firstName.get(), lastName.get(), 
+			   program, firstName.get(), firstSem, course.get(0), firstName.get(), courseGrade.get(0), gender.toLowerCase());
+	if(course.size()>1) {
+		out+= formatCourse();
+	}
+	out+=formatAcaChara();
+	out+=formatPerChara();
+	
+	out+= String.format("Furthermore, I noticed from the term project result, %s developed leadership, time \r\n"
+			+ "management, and problem-solving skills. %s worked effectively with the team members and \r\n"
+			+ "delegated tasks appropriately. They were able to deliver a successful project in a timely fashion.\r\n\n"
+			+ "I believe that %s has the capacity to excel at higher education program and this \r\n"
+			+ "is my pleasure to highly recommend him. \r\n\n"
+			+ "Please do not hesitate to contact me with further questions.\r\n\n\n\n"
+			+ "Very Respectfully,", gender.toLowerCase(), gender, firstName.get());
+	return out;
+	}
 }
