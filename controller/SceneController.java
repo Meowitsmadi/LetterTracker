e8c3580e146ee5a892e2fb32a308434f921b213f
William Ngo <willis12367129@gmail.com>
2023-05-02 08:29:07 -0700

    Update SceneController.java

--- controller/SceneController.java
+++ controller/SceneController.java
@@ -4,40 +4,21 @@ package controller;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Optional;
 import java.util.ResourceBundle;
-
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.*;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-
-import org.controlsfx.control.CheckListView;
 
 import application.LoginModel;
 import application.Student;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextArea;
 import javafx.stage.*;
 import sql.AccessFunctions;
 import sql.sqliteDemo;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.text.Text;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
 
 // Defining the SceneController class which deals with switching to various scenes and implements the Initializable interface
 // for creating and populating Combo Boxes
@@ -54,10 +35,6 @@ public class SceneController implements Initializable {
 	
 	static int ID = 0;
 	static boolean isEdit = false;
-	@FXML
-	Button saveBtn = new Button();
-	@FXML
-	private static TextArea viewLorText = new TextArea();
 	@FXML
 	static TextArea lorText = new TextArea();
 	@FXML
@@ -104,7 +81,7 @@ public class SceneController implements Initializable {
 	}
 	public void save(ActionEvent event) throws IOException, SQLException{
 		// get data from Student class
-		sqliteDemo.InsertRecData("recommendations", student.getFirstName(), student.getLastName(), student.getLOR());
+		sqliteDemo.InsertRecData(student);
 		this.switchToHomeScene1(event);
 	}
 
@@ -130,12 +107,6 @@ public class SceneController implements Initializable {
 	public void EditLOR(ActionEvent event) throws SQLException, IOException {
 		AccessFunctions.EditRecommendation(student,ID);
 		this.switchToHomeScene1(event);
-	}
-	
-	public void updateTextArea(String lor) {
-		viewLorText.setText("hi");
-		System.out.println(viewLorText.getText());
-		back.setText("WAAAAAAAAA");
 	}
 
 	@Override
