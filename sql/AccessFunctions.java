d7e172290d785948b2343248e334b252d87a41a5
William Ngo <willis12367129@gmail.com>
2023-05-02 08:23:33 -0700

    Update AccessFunctions.java

--- sql/AccessFunctions.java
+++ sql/AccessFunctions.java
@@ -87,53 +87,102 @@ public class AccessFunctions {
 		return studentData.get(0);
 	}
 	
-	// inserts data from student's form into the DB
-	public static void InsertFormData(Student s) throws SQLException {
+	public static void EditRecommendation(Student s, int entryID) throws SQLException, IOException {
+		// switch to create new lor BUT without the new
+		String last = s.getLastName();
+		String first = s.getFirstName();
+		String gen = s.getGender();
+		String sem = s.getFirstSem();
+		String pro = s.getProgram();
+		String ts = s.getTargetSchool();
+		String year = s.getFirstYear();
+		String date = s.getCurrentDate();
+		String course = s.getCourses();
+		String courseG = s.getCourseGrades();
+		String perC = s.getPersonalChar();
+		String acaC = s.getAcademicChar();
 		Connection conn = DriverManager.getConnection("jdbc:sqlite:letterTrackerInfo.db");
 		PreparedStatement pst = null;
 		
-		String query = "INSERT OR IGNORE INTO recommendations(firstName, lastName, gender, firstSem, program, targetSchool, firstYear, currentDate,"
-					+ " courses, courseGrades, personalChar, academicChar) "
-						+ "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
-		try {
-			pst = conn.prepareStatement(query);
-			pst.setString(1,s.getFirstName());
-			pst.setString(2,s.getLastName());
-			pst.setString(3,s.getGender()); 
-			pst.setString(4,s.getFirstSem());
-			pst.setString(5,s.getProgram()); 
-			pst.setString(6,s.getTargetSchool());
-			pst.setString(7,s.getFirstYear());
-			pst.setString(8,s.getCurrentDate()); 
-			pst.setString(9,s.getCourses()); 
-			pst.setString(10,s.getCourseGrades()); 
-			pst.setString(11,s.getPersonalChar()); 
-			pst.setString(12,s.getAcademicChar()); 
-			pst.executeUpdate();
-		}
-		catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			pst.close();
-		}
-	}
-	
-	public static void EditRecommendation(Student s, int entryID) throws SQLException, IOException {
-		// switch to create new lor BUT without the new
-		System.out.println("first");
-		String last = s.getLastName();
-		String first = s.getFirstName();
-		System.out.println("last");
-		Connection conn = DriverManager.getConnection("jdbc:sqlite:letterTrackerInfo.db");
-		Statement pst = null;
-		
 		// selects from recommendation students
-		String query1 = "UPDATE recommendations SET lastName = ? WHERE id = "+entryID+";";
-		String query2 = "UPDATE recommendations SET firstName = ? WHERE id = "+entryID+";";
+		String query1 = "UPDATE recommendations SET firstName = ? WHERE id = "+entryID+";";
+		String query2 = "UPDATE recommendations SET lastName = ? WHERE id = "+entryID+";";
+		String query3 = "UPDATE recommendations SET gender = ? WHERE id = "+entryID+";";
+		String query4 = "UPDATE recommendations SET firstSem = ? WHERE id = "+entryID+";";
+		String query5 = "UPDATE recommendations SET program = ? WHERE id = "+entryID+";";
+		String query6 = "UPDATE recommendations SET targetSchool = ? WHERE id = "+entryID+";";
+		String query7 = "UPDATE recommendations SET firstYear = ? WHERE id = "+entryID+";";
+		String query8 = "UPDATE recommendations SET currentDate = ? WHERE id = "+entryID+";";
+		String query9 = "UPDATE recommendations SET courses = ? WHERE id = "+entryID+";";
+		String query10 = "UPDATE recommendations SET courseGrades = ? WHERE id = "+entryID+";";
+		String query11 = "UPDATE recommendations SET personalChar = ? WHERE id = "+entryID+";";
+		String query12 = "UPDATE recommendations SET academicChar = ? WHERE id = "+entryID+";";
+		String query13 = "UPDATE recommendations SET LOR = ? WHERE id = "+entryID+";";
 		try {
-			pst = conn.createStatement();
-			pst.executeUpdate(query1);
-			pst.executeUpdate(query2);
+			pst = conn.prepareStatement(query1);
+			pst.setString(1, first); // selects when searchedName = LastName in DB
+			pst.executeUpdate();
+			pst.close();
+
+			pst = conn.prepareStatement(query2);
+			pst.setString(1, last); // selects when searchedName = LastName in DB
+			pst.executeUpdate();
+			pst.close();
+			
+			pst = conn.prepareStatement(query3);
+			pst.setString(1, gen); // selects when searchedName = LastName in DB
+			pst.executeUpdate();
+			pst.close();
+			
+			pst = conn.prepareStatement(query4);
+			pst.setString(1, sem); // selects when searchedName = LastName in DB
+			pst.executeUpdate();
+			pst.close();
+			
+			pst = conn.prepareStatement(query5);
+			pst.setString(1, pro); // selects when searchedName = LastName in DB
+			pst.executeUpdate();
+			pst.close();
+			
+			pst = conn.prepareStatement(query6);
+			pst.setString(1, ts); // selects when searchedName = LastName in DB
+			pst.executeUpdate();
+			pst.close();
+			
+			pst = conn.prepareStatement(query7);
+			pst.setString(1, year); // selects when searchedName = LastName in DB
+			pst.executeUpdate();
+			pst.close();
+			
+			pst = conn.prepareStatement(query8);
+			pst.setString(1, date); // selects when searchedName = LastName in DB
+			pst.executeUpdate();
+			pst.close();
+			
+			pst = conn.prepareStatement(query9);
+			pst.setString(1, course); // selects when searchedName = LastName in DB
+			pst.executeUpdate();
+			pst.close();
+			
+			pst = conn.prepareStatement(query10);
+			pst.setString(1, courseG); // selects when searchedName = LastName in DB
+			pst.executeUpdate();
+			pst.close();
+			
+			pst = conn.prepareStatement(query11);
+			pst.setString(1, perC); // selects when searchedName = LastName in DB
+			pst.executeUpdate();
+			pst.close();
+			
+			pst = conn.prepareStatement(query12);
+			pst.setString(1, acaC); // selects when searchedName = LastName in DB
+			pst.executeUpdate();
+			pst.close();
+			
+			pst = conn.prepareStatement(query13);
+			pst.setString(1, s.toString()); // selects when searchedName = LastName in DB
+			pst.executeUpdate();
+			pst.close();
 		}
 		catch (SQLException e) {
 			e.printStackTrace();
