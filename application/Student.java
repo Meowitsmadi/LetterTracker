8c4765ca41c767d43936627f9b4b103b8c422b33
William Ngo <willis12367129@gmail.com>
2023-05-02 18:01:32 -0700

    Update Student.java

--- application/Student.java
+++ application/Student.java
@@ -1,11 +1,13 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import sql.sqliteDemo;
 
 public class Student {
 	private IntegerProperty id;
@@ -28,6 +30,7 @@ public class Student {
         this.firstName = new SimpleStringProperty(firstName);
         this.lastName = new SimpleStringProperty(lastName);
     }
+
 	
 	public Student(String firstName, String lastName, String gender, String firstSem, String program, String targetSchool,
 			String firstYear, String currentDate, ArrayList<String> course, ArrayList<String> courseGrade,
@@ -52,10 +55,12 @@ public class Student {
 		this.courseGrade = courseGrade;
 		this.personalChar = personalChar;
 		this.academicChar = academicChar;
+		lor = this.getString();
     }
 	
     public Student() {}
 	
+
 	public IntegerProperty idProperty() { 
          if (id == null) id = new SimpleIntegerProperty(this, "id");
          return id; 
@@ -109,67 +114,94 @@ public class Student {
 		return out;}
 
 	public String formatCourse() {
-	String out ="";
+	String out ="\n";
 	int i = 1;
 	out += String.format("%s also earned ", gender);
 	while(i<course.size()) {
-		if(i!=course.size()-1) { 
-			out+=String.format("%s from my %s, ", courseGrade.get(i), course.get(i));
+		if(course.size()==2) {
+			out+=String.format("'%s' from my '%s'", courseGrade.get(i), course.get(i));
 		}
-		else { out+=String.format("and %s from my %s", courseGrade.get(i), course.get(i)); }
+		else if(i!=course.size()-1){
+			out+=String.format("'%s' from my '%s', ", courseGrade.get(i), course.get(i));	
+		}
+		else { out+=String.format(",and '%s' from my '%s'", courseGrade.get(i), course.get(i)); }
 		i++;
 	}
 	return out+".\n";
 	}
 
-public String formatAcaChara() {
-	String out = String.format("%s %s",firstName.get(), academicChar.get(0));
-	for(int i=1; i<academicChar.size();i++) {
-		if(i!=course.size()-1) { 
-			out+= String.format("%s, ", academicChar.get(i));
+	public String formatAcaChara() {
+		String out = String.format("\n%s %s",firstName.get(), academicChar.get(0));
+		for(int i=1; i<academicChar.size();i++) {
+			if(i!=academicChar.size()-1) { 
+				out+= String.format(", %s", academicChar.get(i));
+			}
+			else { out+= String.format(", and %s", academicChar.get(i)); }	
 		}
-		else { out+= String.format("and %s", academicChar.get(i)); }	
-	}
-	return out+".\n\n";
+		return out+".\n\n";
 	
-}
+	}
 
-public String formatPerChara() {
-	String out = String.format("%s was always %s", gender, personalChar.get(0));
-	for(int i=1; i<personalChar.size();i++) {
-		if(i!=course.size()-1) { 
-			out+= String.format("%s, ", personalChar.get(i));
+	public String formatPerChara() {
+		String out = String.format("%s was always %s", gender, personalChar.get(0));
+		for(int i=1; i<personalChar.size();i++) {
+			if(i!=personalChar.size()-1) { 
+				out+= String.format(", %s", personalChar.get(i));
+			}
+			else { out+= String.format(", and %s", personalChar.get(i)); }	
 		}
-		else { out+= String.format("and %s.\n", personalChar.get(i)); }	
+		return out+".\n\n";
+	
 	}
-	return out+".\n\n";
 	
-}
+	public String signature() {
+		ArrayList<String> data = sqliteDemo.getAllData("userData");
+		String out = data.get(0);
+		out += String.format("\n\n%s\n%s\n%s\n%s", data.get(1), data.get(2), data.get(3), data.get(4));
+		return out;
+	}
 	
-	public String toString() {
+	public String altGender() {
+		if(gender.equals("He")) {
+			return "him";
+		}
+		return "her";
+	}
+	
+	public void setLor(String lor) {
+		this.lor = lor;
+	}
+	
+	public String getLor() {
+		return lor;
+	}
+	
+	private String getString() {
 		String out = String.format(""
 			+ "Letter of Recommendation\r\n\n"
 			+ "For: %s %s\r\n"
 			+ "Date: %s\r\n"
 			+ "To: Graduate Admissions Committee\r\n\n"
-			+ "I am writing this letter to recommend my former student %s %s who is applying for the\r\n"
+			+ "I am writing this letter to recommend my former student %s %s who is applying for the "
 			+ "%s program in your school. \r\n\n"
-			+ "I met %s in %s when he enrolled in my \"%s\" course.\r\n\n"
+			+ "I met %s %s in %s when %s enrolled in my \"%s\" course.\r\n\n"
 			+ "%s earned \"%s\" from this tough course, and this shows how knowledgeable and hard worker %s is. \r\n", firstName.get(), lastName.get(), currentDate, firstName.get(), lastName.get(), 
-			   program, firstName.get(), firstSem, course.get(0), firstName.get(), courseGrade.get(0), gender.toLowerCase());
+			   program, firstName.get(), firstSem, firstYear, gender.toLowerCase(), course.get(0), firstName.get(), courseGrade.get(0), gender.toLowerCase());
 	if(course.size()>1) {
 		out+= formatCourse();
 	}
 	out+=formatAcaChara();
 	out+=formatPerChara();
 	
-	out+= String.format("Furthermore, I noticed from the term project result, %s developed leadership, time \r\n"
-			+ "management, and problem-solving skills. %s worked effectively with the team members and \r\n"
+	out+= String.format("Furthermore, I noticed from the term project result, %s developed leadership, time "
+			+ "management, and problem-solving skills. %s worked effectively with the team members and "
 			+ "delegated tasks appropriately. They were able to deliver a successful project in a timely fashion.\r\n\n"
-			+ "I believe that %s has the capacity to excel at higher education program and this \r\n"
-			+ "is my pleasure to highly recommend him. \r\n\n"
-			+ "Please do not hesitate to contact me with further questions.\r\n\n\n\n"
-			+ "Very Respectfully,", gender.toLowerCase(), gender, firstName.get());
+			+ "I believe that %s has the capacity to excel at higher education program and this "
+			+ "is my pleasure to highly recommend %s. \r\n\n"
+			+ "Please do not hesitate to contact me with further questions.\r\n\n\n"
+			+ "Very Respectfully,\n\n", gender.toLowerCase(), gender, firstName.get(), altGender());
+	
+	out+= signature();
 	return out;
 	}
 }
