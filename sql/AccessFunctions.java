83c6bb03d610a26471054d53eb7b0db00dff3047
Madison Kolley <53013984+Meowitsmadi@users.noreply.github.com>
2023-05-04 18:25:16 -0700

    Update AccessFunctions.java

--- sql/AccessFunctions.java
+++ sql/AccessFunctions.java
@@ -15,12 +15,11 @@ import javafx.collections.ObservableList;
 
 public class AccessFunctions {
 
-	
+	// Deletes the recommendation in the database associated with the student's ID.
 	public static void DeleteRecommendation(int entryID) throws SQLException, IOException {
 		Connection conn = DriverManager.getConnection("jdbc:sqlite:letterTrackerInfo.db");
 		PreparedStatement pst = null;
 		
-		// deletes from the DB based on ID
 		String query = "DELETE FROM recommendations WHERE id = ?"; 
 		try {
 			pst = conn.prepareStatement(query);
@@ -34,6 +33,7 @@ public class AccessFunctions {
 		}
 	}
 	
+	// returns a list of students where the searched name equals the last name in the database.
 	public static ObservableList<Student> getData(String searchedName) throws SQLException, IOException {
 		ObservableList<Student> studentData = FXCollections.observableArrayList();
 		Connection conn = DriverManager.getConnection("jdbc:sqlite:letterTrackerInfo.db");
@@ -61,6 +61,7 @@ public class AccessFunctions {
 		return studentData;
 	}
 	
+	// // returns a student with the associated ID from the database.
 	public static Student getData(int id) throws SQLException, IOException {
 		ObservableList<Student> studentData = FXCollections.observableArrayList();
 		Connection conn = DriverManager.getConnection("jdbc:sqlite:letterTrackerInfo.db");
@@ -71,9 +72,9 @@ public class AccessFunctions {
 		String query = "SELECT * FROM recommendations WHERE ID = ?";
 		try {
 			pst = conn.prepareStatement(query);
-			pst.setInt(1, id); // selects when searchedName = LastName in DB
+			pst.setInt(1, id); // selects when id = ID in the database.
 			rs = pst.executeQuery();
-			List<String> course = Arrays.asList(rs.getString("courses").split("/"));
+			List<String> course = Arrays.asList(rs.getString("courses").split("/")); // splits up data into multiple strings
 			List<String> courseG = Arrays.asList(rs.getString("courseGrades").split("/"));
 			List<String> perC = Arrays.asList(rs.getString("personalChar").split("/"));
 			List<String> acaC = Arrays.asList(rs.getString("academicChar").split("/"));
@@ -92,9 +93,10 @@ public class AccessFunctions {
 			pst.close();
 		}
 		System.out.println(studentData.size());
-		return studentData.get(0);
+		return studentData.get(0); // gets the first student in the list and returns it. 
 	}
 	
+	// Updates the previous form data in the database.
 	public static void EditRecommendation(Student s, int entryID) throws SQLException, IOException {
 		// switch to create new lor BUT without the new
 		String last = s.getLastName();
@@ -112,7 +114,7 @@ public class AccessFunctions {
 		Connection conn = DriverManager.getConnection("jdbc:sqlite:letterTrackerInfo.db");
 		PreparedStatement pst = null;
 		
-		// selects from recommendation students
+		// updates current data in the recommendations database
 		String query1 = "UPDATE recommendations SET firstName = ? WHERE id = "+entryID+";";
 		String query2 = "UPDATE recommendations SET lastName = ? WHERE id = "+entryID+";";
 		String query3 = "UPDATE recommendations SET gender = ? WHERE id = "+entryID+";";
@@ -128,67 +130,67 @@ public class AccessFunctions {
 		String query13 = "UPDATE recommendations SET LOR = ? WHERE id = "+entryID+";";
 		try {
 			pst = conn.prepareStatement(query1);
-			pst.setString(1, first); // selects when searchedName = LastName in DB
+			pst.setString(1, first); // updates the first name in the database
 			pst.executeUpdate();
 			pst.close();
 
 			pst = conn.prepareStatement(query2);
-			pst.setString(1, last); // selects when searchedName = LastName in DB
+			pst.setString(1, last); // updates the last name in the database
 			pst.executeUpdate();
 			pst.close();
 			
 			pst = conn.prepareStatement(query3);
-			pst.setString(1, gen); // selects when searchedName = LastName in DB
+			pst.setString(1, gen); // updates the gender in the database
 			pst.executeUpdate();
 			pst.close();
 			
 			pst = conn.prepareStatement(query4);
-			pst.setString(1, sem); // selects when searchedName = LastName in DB
+			pst.setString(1, sem); // updates the student's first semester in the database
 			pst.executeUpdate();
 			pst.close();
 			
 			pst = conn.prepareStatement(query5);
-			pst.setString(1, pro); // selects when searchedName = LastName in DB
+			pst.setString(1, pro); // updates the student's program in the database
 			pst.executeUpdate();
 			pst.close();
 			
 			pst = conn.prepareStatement(query6);
-			pst.setString(1, ts); // selects when searchedName = LastName in DB
+			pst.setString(1, ts); // updates the student's target school in the database
 			pst.executeUpdate();
 			pst.close();
 			
 			pst = conn.prepareStatement(query7);
-			pst.setString(1, year); // selects when searchedName = LastName in DB
+			pst.setString(1, year); // updates the student's first year in the database
 			pst.executeUpdate();
 			pst.close();
 			
 			pst = conn.prepareStatement(query8);
-			pst.setString(1, date); // selects when searchedName = LastName in DB
+			pst.setString(1, date); // updates the date in the database
 			pst.executeUpdate();
 			pst.close();
 			
 			pst = conn.prepareStatement(query9);
-			pst.setString(1, course); // selects when searchedName = LastName in DB
+			pst.setString(1, course); // updates the student's course in the database
 			pst.executeUpdate();
 			pst.close();
 			
 			pst = conn.prepareStatement(query10);
-			pst.setString(1, courseG); // selects when searchedName = LastName in DB
+			pst.setString(1, courseG); // updates the student's course grade in the database
 			pst.executeUpdate();
 			pst.close();
 			
 			pst = conn.prepareStatement(query11);
-			pst.setString(1, perC); // selects when searchedName = LastName in DB
+			pst.setString(1, perC); // updates the student's personal characteristics in the database
 			pst.executeUpdate();
 			pst.close();
 			
 			pst = conn.prepareStatement(query12);
-			pst.setString(1, acaC); // selects when searchedName = LastName in DB
+			pst.setString(1, acaC); // updates the student's academic characteristics in the database
 			pst.executeUpdate();
 			pst.close();
 			
 			pst = conn.prepareStatement(query13);
-			pst.setString(1, s.toString()); // selects when searchedName = LastName in DB
+			pst.setString(1, s.toString()); // updates the letter of recommendation text in the database
 			pst.executeUpdate();
 			pst.close();
 		}
