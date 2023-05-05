7f2807f0ce033b0c3823c541cbaa7bcea136d81c
prathj <70483394+prathj@users.noreply.github.com>
2023-05-04 19:10:34 -0700

    Create StudentGrade.java

---
+++ application/StudentGrade.java
@@ -0,0 +1,22 @@
+package application;
+
+import javafx.scene.control.TextField;
+
+public class StudentGrade {
+		private String course;
+		private TextField grade;
+		
+		public StudentGrade(String course, TextField grade) {
+			this.course = course;
+			this.grade = grade;
+		}
+		
+		public TextField getGrade() {
+			return grade;
+		}
+		
+		public String getCourse() {
+			return course;
+		}
+		
+}
